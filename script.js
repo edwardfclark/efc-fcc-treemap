7c7fa8ffc0d1f37102f1d27d7fa38099cac154fc
Edward Clark <edwardfclark@gmail.com>
2018-12-07 17:37:04 +0000

    Major Changes
    - Got the color scale working.
    - Added basic tooltips.
    - Added menu with chart-swapping functionality.
    
    Wrestled with some bugs regarding async stuff and creating the #tooltip
    div, but solved them by including the call in window.onload.

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const PADDING = {top: 50, right: 0, bottom: 0, left: 0}
 const PADDING_OUTER = 3;
 const PADDING_INNER = 3;
 
+const tooltipWidth = 140;
+const tooltipHeight = 55;
+let tooltipDiv;
 
-
-console.log("starting the call...");
-
+//This call populates the page with the Video Games Dataset treemap on page load.
 //This is inside window.onload to give access to the DOM inside the function.
 window.onload = () => {
     buildTreemap("https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json");
+    tooltipDiv = d3.select("body").append("div").attr("id", "tooltip").style("opacity", 0).style("width", tooltipWidth+"px");
+
 }
 
 
@@ -29,6 +32,7 @@ function buildTreemap(url) {
     })
     .then((json) => {
         let treemapLayout = d3.treemap().size([WIDTH, HEIGHT]).paddingOuter(PADDING_OUTER).paddingInner(PADDING_INNER);
+        let color = d3.scaleOrdinal(d3.schemePaired);
         let root = d3.hierarchy(json);
         root.sum((d) => d.value);
         treemapLayout(root);
@@ -48,8 +52,16 @@ function buildTreemap(url) {
             nodes.append("rect")
             .attr("width", (d) => d.x1-d.x0)
             .attr("height", (d) => d.y1-d.y0)
-            .attr("fill", "blue")
-            .style("opacity", .3);
+            .attr("fill", (d) => color(d.data.category))
+            .style("opacity", 1)
+            .on("mouseover", (d) => {
+                let data = d.data;
+                tooltipDiv.transition().duration(100).style("opacity", 1);
+                tooltipDiv.html(generateTooltip(data))
+                // .attr(/*data attributes*/)
+                   .style("left", (d3.event.pageX)+"px")
+                   .style("top", (d3.event.pageY-tooltipHeight)+"px");
+            });
     
             nodes.append("text")
             .attr("dx", 4)
@@ -57,5 +69,15 @@ function buildTreemap(url) {
             .text((d) => d.data.name);
         
     });
+    
+}
+
+function generateTooltip(data) {
+    let html = `<p>
+            Name: ${data.name}<br/>
+            Category: ${data.category}<br/>
+            Value: ${data.value}
+            </p>`;
+    return html;
     
 }
